26e969b3f54251d9c9c25d436fc8ccb1337d19b0
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-05-17 18:38:40 -0500

    Add audio and custom font

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by William McDermott on 5/17/17.
+//  Copyright © 2017 William McDermott. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by William McDermott on 5/17/17.
+//  Copyright © 2017 William McDermott. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
